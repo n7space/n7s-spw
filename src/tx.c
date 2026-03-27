a36da26326090b7204d7a8a1e3647992e4923462
Filip Florczyk <filip.florczyk@n7space.com>
2026-03-27 08:26:29 +0100

    Added TX implementation

--- src/tx.c
+++ src/tx.c
@@ -1,37 +1,211 @@
 // N7 Space Sp. z o.o.
 // https://n7space.com
-// COPYRIGHT (C) 2022-2025
+// COPYRIGHT (C) 2022-2026
 
 #include "tx.h"
 
-void Spw_Tx_init(){}
+#define SWRESET_ARM_PATTERN     0x4D616A6Fu
+#define SWRESET_TRIGGER_PATTERN 0x72546F6Du
 
-void Spw_Tx_setConfig(){}
+#define SPW_PKTTX1_IRQREGS_ALLIRQ_MASK 0x0000000Fu
 
-void Spw_Tx_reset(){}
+void Spw_Tx_init(Spw_Tx *const tx) { (void)tx; }
 
-void Spw_Tx_setNextSendList(){}
+void Spw_Tx_setConfig(Spw_Tx *const tx, const Spw_Tx_Config *const config)
+{
+	(void)tx; // Suppress unused parameter warning
 
-void Spw_Tx_setNextSendListEntry(){}
+	SPW_PKTTX_InterruptEnable(config->irqMaskEnable);
+	SPW_PKTTX_InterruptDisable(config->irqMaskDisable);
+}
 
-void Spw_Tx_abortCurrentSendList(){}
+void Spw_Tx_reset(const Spw_Tx *const tx)
+{
+	(void)tx; // Suppress unused parameter warning
 
-void Spw_Tx_getCurrentSendListConfig(){}
+	SPW_REGS->SPW_PKTTX1_SWRESET = SWRESET_ARM_PATTERN;
+	// Generate read from SWRESET register (without it resets are unreliable)
+	(void)SPW_REGS->SPW_PKTTX1_SWRESET;
+	SPW_REGS->SPW_PKTTX1_SWRESET = SWRESET_TRIGGER_PATTERN;
+}
 
-void Spw_Tx_getSendListEntry(){}
+void Spw_Tx_setNextSendList(Spw_Tx *const tx,
+			    const Spw_Tx_SendListConfig *const config)
+{
+	(void)tx; // Suppress unused parameter warning
 
-void Spw_Tx_setIrqMask(){}
+	SPW_PKTTX_SetNextSendList(
+		(uint8_t *)config->routerByte,
+		config->sendListAddress,
+		config->sendListLength,
+		config->abortOngoingSendListWhenStarted,
+		(SPW_PKTTX_NXTSEND_START)config->sendCondition,
+		config->startValue);
+}
 
-uint32_t Spw_Tx_getIrq(){ return 0; }
+void Spw_Tx_setNextSendListEntry(Spw_Tx_SendListEntry *const entry,
+				 const Spw_Tx_SendListEntryStruct *const entryStruct)
+{
+	entry->Skip    = entryStruct->isEntrySkipped ? 1U : 0U;
+	entry->Entry   = (uint32_t)entryStruct->entryType;
+	entry->RSize   = (uint32_t)entryStruct->routerByteLength;
+	entry->Start   = (uint32_t)entryStruct->startTime;
+	entry->RB1     = (uint32_t)entryStruct->routerByte[0];
+	entry->RB2     = (uint32_t)entryStruct->routerByte[1];
+	entry->RB3     = (uint32_t)entryStruct->routerByte[2];
+	entry->RB4     = (uint32_t)entryStruct->routerByte[3];
+	entry->RB5     = (uint32_t)entryStruct->routerByte[4];
+	entry->RB6     = (uint32_t)entryStruct->routerByte[5];
+	entry->RB7     = (uint32_t)entryStruct->routerByte[6];
+	entry->RB8     = (uint32_t)entryStruct->routerByte[7];
+	entry->EscMask = (uint32_t)entryStruct->escapeCharMask;
+	entry->EscChar = (uint32_t)entryStruct->escapeChar;
+	entry->HCrc    = entryStruct->calculateHeaderCrc ? 1U : 0U;
+	entry->HSize   = (uint32_t)entryStruct->headerSize;
+	entry->HAddr   = (uint32_t)entryStruct->headerAddress;
+	entry->DCrc    = entryStruct->calculateDataCrc ? 1U : 0U;
+	entry->DSize   = (uint32_t)entryStruct->dataSize;
+	entry->DAddr   = (uint32_t)entryStruct->dataAddress;
+	entry->TimeOut = (uint32_t)entryStruct->timeout;
+}
 
-uint32_t Spw_Tx_getIrqMasked(){ return 0; }
+void Spw_Tx_abortCurrentSendList(Spw_Tx *const tx)
+{
+	(void)tx; // Suppress unused parameter warning
 
-uint32_t Spw_Tx_getAndClearIrq(){ return 0; }
+	SPW_REGS->SPW_PKTTX1_CURSENDCFG |= SPW_PKTTX1_CURSENDCFG_ABORT_Msk;
+}
 
-uint32_t Spw_Tx_getAndClearIrqMasked(){ return 0; }
+void Spw_Tx_getCurrentSendListConfig(const Spw_Tx *const tx,
+				     Spw_Tx_SendListConfig *const config)
+{
+	(void)tx; // Suppress unused parameter warning
 
-void Spw_Tx_clearIrq(){}
+	uint32_t curSendCfg  = SPW_REGS->SPW_PKTTX1_CURSENDCFG;
+	uint32_t curSendRout = SPW_REGS->SPW_PKTTX1_CURSENDROUT;
 
-void Spw_Tx_getStructuredIrq(){}
+	config->sendListAddress =
+		(Spw_Tx_SendListEntry *)SPW_REGS->SPW_PKTTX1_CURSENDADDR;
+	config->sendListLength =
+		(uint16_t)((curSendCfg & SPW_PKTTX1_CURSENDCFG_LEN_Msk) >>
+			   SPW_PKTTX1_CURSENDCFG_LEN_Pos);
+	config->routerByte[0] =
+		(uint8_t)((curSendRout & SPW_PKTTX1_NXTSENDROUT_BYTE1_Msk) >>
+			  SPW_PKTTX1_NXTSENDROUT_BYTE1_Pos);
+	config->routerByte[1] =
+		(uint8_t)((curSendRout & SPW_PKTTX1_NXTSENDROUT_BYTE2_Msk) >>
+			  SPW_PKTTX1_NXTSENDROUT_BYTE2_Pos);
+	config->routerByte[2] =
+		(uint8_t)((curSendRout & SPW_PKTTX1_NXTSENDROUT_BYTE3_Msk) >>
+			  SPW_PKTTX1_NXTSENDROUT_BYTE3_Pos);
+	config->routerByte[3] =
+		(uint8_t)((curSendRout & SPW_PKTTX1_NXTSENDROUT_BYTE4_Msk) >>
+			  SPW_PKTTX1_NXTSENDROUT_BYTE4_Pos);
+	// sendCondition, startValue, abortOngoingSendListWhenStarted are not
+	// available in current send list registers; set to defaults.
+	config->sendCondition		       = (Spw_Tx_SendCondition)0;
+	config->startValue		       = 0U;
+	config->abortOngoingSendListWhenStarted = false;
+}
 
-void Spw_Tx_getStatus(){}
+void Spw_Tx_getSendListEntry(const Spw_Tx_SendListEntry *const entry,
+			     Spw_Tx_SendListEntryStruct *const entryStruct)
+{
+	entryStruct->isEntrySkipped    = entry->Skip != 0U;
+	entryStruct->entryType	       = (Spw_Tx_EntryType)entry->Entry;
+	entryStruct->routerByteLength  = (uint8_t)entry->RSize;
+	entryStruct->startTime	       = (uint32_t)entry->Start;
+	entryStruct->routerByte[0]     = (uint8_t)entry->RB1;
+	entryStruct->routerByte[1]     = (uint8_t)entry->RB2;
+	entryStruct->routerByte[2]     = (uint8_t)entry->RB3;
+	entryStruct->routerByte[3]     = (uint8_t)entry->RB4;
+	entryStruct->routerByte[4]     = (uint8_t)entry->RB5;
+	entryStruct->routerByte[5]     = (uint8_t)entry->RB6;
+	entryStruct->routerByte[6]     = (uint8_t)entry->RB7;
+	entryStruct->routerByte[7]     = (uint8_t)entry->RB8;
+	entryStruct->escapeCharMask    = (uint8_t)entry->EscMask;
+	entryStruct->escapeChar	       = (uint8_t)entry->EscChar;
+	entryStruct->calculateHeaderCrc = entry->HCrc != 0U;
+	entryStruct->headerSize	       = (uint8_t)entry->HSize;
+	entryStruct->headerAddress     = (uint8_t *)entry->HAddr;
+	entryStruct->calculateDataCrc  = entry->DCrc != 0U;
+	entryStruct->dataSize	       = (uint32_t)entry->DSize;
+	entryStruct->dataAddress       = (uint8_t *)entry->DAddr;
+	entryStruct->timeout	       = (uint32_t)entry->TimeOut;
+}
+
+void Spw_Tx_setIrqMask(Spw_Tx *const tx, uint32_t mask)
+{
+	(void)tx; // Suppress unused parameter warning
+
+	SPW_REGS->SPW_PKTTX1_IM = mask & SPW_PKTTX1_IRQREGS_ALLIRQ_MASK;
+}
+
+uint32_t Spw_Tx_getIrq(const Spw_Tx *const tx)
+{
+	(void)tx; // Suppress unused parameter warning
+
+	return SPW_REGS->SPW_PKTTX1_PI_R & SPW_PKTTX1_IRQREGS_ALLIRQ_MASK;
+}
+
+uint32_t Spw_Tx_getIrqMasked(const Spw_Tx *const tx)
+{
+	(void)tx; // Suppress unused parameter warning
+
+	return SPW_REGS->SPW_PKTTX1_PI_RM & SPW_PKTTX1_IRQREGS_ALLIRQ_MASK;
+}
+
+uint32_t Spw_Tx_getAndClearIrq(const Spw_Tx *const tx)
+{
+	(void)tx; // Suppress unused parameter warning
+
+	uint32_t irq = SPW_REGS->SPW_PKTTX1_PI_R & SPW_PKTTX1_IRQREGS_ALLIRQ_MASK;
+	SPW_REGS->SPW_PKTTX1_PI_C = irq;
+	return irq;
+}
+
+uint32_t Spw_Tx_getAndClearIrqMasked(const Spw_Tx *const tx)
+{
+	(void)tx; // Suppress unused parameter warning
+
+	return SPW_PKTTX_IrqStatusGetMaskedAndClear() &
+	       SPW_PKTTX1_IRQREGS_ALLIRQ_MASK;
+}
+
+void Spw_Tx_clearIrq(const Spw_Tx *const tx)
+{
+	(void)tx; // Suppress unused parameter warning
+
+	SPW_REGS->SPW_PKTTX1_PI_C = SPW_PKTTX1_IRQREGS_ALLIRQ_MASK;
+}
+
+void Spw_Tx_getStructuredIrq(const uint32_t linkIrqStatusValue,
+			     Spw_Tx_IrqStatus *const status)
+{
+	status->eepSentIrqOccurred =
+		(linkIrqStatusValue & (uint32_t)SPW_PKTTX_INT_MASK_EEP) != 0U;
+	status->eopSentIrqOccurred =
+		(linkIrqStatusValue & (uint32_t)SPW_PKTTX_INT_MASK_EOP) != 0U;
+	status->sendListActivatedIrqOccurred =
+		(linkIrqStatusValue & (uint32_t)SPW_PKTTX_INT_MASK_ACT) != 0U;
+	status->sendListDeactivatedIrqOccurred =
+		(linkIrqStatusValue & (uint32_t)SPW_PKTTX_INT_MASK_DEACT) != 0U;
+}
+
+void Spw_Tx_getStatus(Spw_Tx *const tx, Spw_Tx_Status *const status)
+{
+	(void)tx; // Suppress unused parameter warning
+
+	SPW_PKTTX_STATUS rawStatus = SPW_PKTTX_StatusGet();
+
+	status->lastState =
+		(Spw_Tx_DeactivatedSendListState)SPW_PKTTX_STATUS_PREV_GET(
+			rawStatus);
+	status->isReadyToDeactivateCurrentSendList =
+		(rawStatus & SPW_PKTTX_STATUS_DEACT) != 0U;
+	status->isNextSendListPending =
+		(rawStatus & SPW_PKTTX_STATUS_PENDING) != 0U;
+	status->isCurrentSendListActive =
+		(rawStatus & SPW_PKTTX_STATUS_ACT) != 0U;
+	status->isNextSendListArmed = (rawStatus & SPW_PKTTX_STATUS_ARM) != 0U;
+}
--- src/tx.h
+++ src/tx.h
@@ -13,54 +13,199 @@ extern "C" {
 #endif
 
 #include "stdint.h"
+#include "stdbool.h"
+
+#include "microchip_spw/plib_spw_pkttx.h"
+
+/// \brief Structure representing a Spacewire tx instance.
+typedef struct {
+  uint32_t dummy;
+} Spw_Tx;
+
+/// \brief Spw Send List tx condition.
+typedef enum {
+  /// \brief Start if any bit in Start Value matches an incoming event.
+  Spw_Tx_SendCondition_StartValueMatch = 0U,
+  /// \brief Start immediately, if possible.
+  Spw_Tx_SendCondition_StartNow = 1U,
+  /// \brief Start if Start Value matches an incoming Time Code from Time Code Handler 1
+  Spw_Tx_SendCondition_StartValueMatchTch1 = 2U,
+  /// \brief Force the compiler to reserve 32-bit memory for enum
+  Spw_Tx_SendCondition_Invalid = 0xFFFFFFFFUL
+} Spw_Tx_SendCondition;
+
+/// \brief Packet entry type.
+typedef enum {
+  /// \brief Normal data packet.
+  Spw_Tx_EntryType_PacketData = 0u,
+  /// \brief Packet contains escape character.
+  Spw_Tx_EntryType_EscapeCharacter = 1u,
+} Spw_Tx_EntryType;
+
+/// \brief Send List packet entry configuration structure.
+typedef struct {
+  /// \brief Spw skips this entry.
+  bool isEntrySkipped;
+  /// \brief Packet entry type.
+  Spw_Tx_EntryType entryType;
+  /// \brief Number of router bytes.
+  uint8_t routerByteLength;
+  /// \brief Delay from send list start. Time Unit is environment dependent.
+  uint32_t startTime;
+  /// \brief Router bytes to use. Number of used entries depends on routerByteLength field
+  uint8_t routerByte[8];
+  /// \brief Defines where to send Escape Character.
+  uint8_t escapeCharMask;
+  /// \brief Escape Character to send if Entry Type is Escape Char.
+  uint8_t escapeChar;
+  /// \brief Enable header CRC calculation.
+  bool calculateHeaderCrc;
+  /// \brief Header size in bytes.
+  uint8_t headerSize;
+  /// \brief Pointer to the header. Must be aligned to 4 bytes.
+  uint8_t* headerAddress;
+  /// \brief Enable data CRC calculation.
+  bool calculateDataCrc;
+  /// \brief Size of data field
+  uint32_t dataSize;
+  /// \brief Pointer to the data. Must be aligned to 4 bytes.
+  uint8_t* dataAddress;
+  /// \brief Abort send list if time from send list start exceeds this value. Write 0 to disable.
+  uint32_t timeout;
+} Spw_Tx_SendListEntryStruct;
+
+/// \brief Send List entry buffer.
+typedef SPW_PKTTX_SEND_LIST_ENTRY Spw_Tx_SendListEntry;
+
+/// \brief Enumeration listing possible Deactivated Send List states.
+typedef enum {
+  /// \brief No information. Field not locked.
+  Spw_Tx_DeactivatedSendListState_Noinfo = 0,
+  /// \brief Last send list fully sent.
+  Spw_Tx_DeactivatedSendListState_LastSendIsOk = 1,
+  /// \brief Aborted due to memory access error.
+  Spw_Tx_DeactivatedSendListState_AbortedMemoryAccessError = 2,
+  /// \brief Aborted by new send list.
+  Spw_Tx_DeactivatedSendListState_AbortedNewSendList = 3,
+  /// \brief Aborted by direct user command.
+  Spw_Tx_DeactivatedSendListState_AbortedByUser = 4,
+  /// \brief Aborted by timeout.
+  Spw_Tx_DeactivatedSendListState_Timeout = 5,
+  /// \brief Number of possible states.
+  Spw_Tx_DeactivatedSendListState_Count = 6
+} Spw_Tx_DeactivatedSendListState;
+
+/// \brief Structure describing PktTx status.
+typedef struct {
+  /// \brief Previous Send List state
+  Spw_Tx_DeactivatedSendListState lastState;
+  /// \brief Flag indicating whether current send list can be aborted.
+  bool isReadyToDeactivateCurrentSendList;
+  /// \brief Is next Send List pending.
+  bool isNextSendListPending;
+  /// \brief Is current Send List active.
+  bool isCurrentSendListActive;
+  /// \brief Is next Send List armed.
+  bool isNextSendListArmed;
+} Spw_Tx_Status;
+
+/// \brief Send List configuration structure.
+typedef struct {
+  /// \brief Send List send condition.
+  Spw_Tx_SendCondition sendCondition;
+  /// \brief Number of entries to send in Send List.
+  uint16_t sendListLength;
+  /// \brief Pointer to the Send List entries.
+  Spw_Tx_SendListEntry* sendListAddress;
+  /// \brief Router bytes to add if non-zero.
+  uint8_t routerByte[4];
+  /// \brief Abort current Send List if new is started.
+  bool abortOngoingSendListWhenStarted;
+  /// \brief Activate on matching Start value. Used when with Spw_Tx_SendCondition_StartValueMatch.
+  uint8_t startValue;
+} Spw_Tx_SendListConfig;
+
+/// \brief Enumeration listing possible PktTx interrupts to be enabled.
+typedef enum {
+  /// \brief Eep was sent.
+  Spw_Tx_Irq_EepSent = SPW_PKTTX_INT_MASK_EEP,
+  /// \brief Eop was sent.
+  Spw_Tx_Irq_EopSent = SPW_PKTTX_INT_MASK_EOP,
+  /// \brief Send List was activated.
+  Spw_Tx_Irq_SendListActivated = SPW_PKTTX_INT_MASK_ACT,
+  /// \brief Send List was deactivated.
+  Spw_Tx_Irq_SendListDeactivated = SPW_PKTTX_INT_MASK_DEACT
+} Spw_Tx_Irq;
+
+/// \brief Structure listing triggered PktTx interrupts.
+typedef struct {
+  /// \brief Eep sent interrupt triggered.
+  bool eepSentIrqOccurred;
+  /// \brief Eop sent interrupt triggered.
+  bool eopSentIrqOccurred;
+  /// \brief Send List activated interrupt triggered.
+  bool sendListActivatedIrqOccurred;
+  /// \brief Send List deactivated interrupt triggered.
+  bool sendListDeactivatedIrqOccurred;
+} Spw_Tx_IrqStatus;
+
+/// \brief PktTx configuration structure.
+typedef struct {
+  /// @brief Interrupt mask for enable.
+	uint32_t irqMaskEnable;
+	/// @brief Interrupt mask for disable.
+	uint32_t irqMaskDisable;uint32_t irqMask;
+} Spw_Tx_Config;
 
 /// \brief Initializes a device descriptor for Spacewire tx.
-void Spw_Tx_init();
+void Spw_Tx_init(Spw_Tx* const tx);
 
 /// \brief Sets Spacewire PktTx config.
-void Spw_Tx_setConfig();
+void Spw_Tx_setConfig(Spw_Tx* const tx, const Spw_Tx_Config* const config);
 
 /// \brief Resets Spacewire PktTx to default state.
-void Spw_Tx_reset();
+void Spw_Tx_reset(const Spw_Tx* const tx);
 
 /// \brief Sets next Send List.
-void Spw_Tx_setNextSendList();
+void Spw_Tx_setNextSendList(Spw_Tx* const tx, const Spw_Tx_SendListConfig* const config);
 
 /// \brief Sets next Send List entry.
-void Spw_Tx_setNextSendListEntry();
+void Spw_Tx_setNextSendListEntry(Spw_Tx_SendListEntry* const entry,
+  const Spw_Tx_SendListEntryStruct* const entryStruct);
 
 /// \brief Aborts transmission of current Send List.
-void Spw_Tx_abortCurrentSendList();
+void Spw_Tx_abortCurrentSendList(Spw_Tx* const tx);
 
 /// \brief Gets current Send List Config.
-void Spw_Tx_getCurrentSendListConfig();
+void Spw_Tx_getCurrentSendListConfig(const Spw_Tx* const tx, Spw_Tx_SendListConfig* const config);
 
 /// \brief Gets Send List entry structure.
-void Spw_Tx_getSendListEntry();
+void Spw_Tx_getSendListEntry(const Spw_Tx_SendListEntry* const entry,
+  Spw_Tx_SendListEntryStruct* const entryStruct);
 
 /// \brief Sets PktTx interrupt mask.
-void Spw_Tx_setIrqMask();
+void Spw_Tx_setIrqMask(Spw_Tx* const tx, uint32_t mask);
 
 /// \brief Gets PktTx interrupts.
-uint32_t Spw_Tx_getIrq();
+uint32_t Spw_Tx_getIrq(const Spw_Tx* const tx);
 
 /// \brief Gets masked PktTx interrupts.
-uint32_t Spw_Tx_getIrqMasked();
+uint32_t Spw_Tx_getIrqMasked(const Spw_Tx* const tx);
 
 /// \brief Gets and clears PktTx interrupts.
-uint32_t Spw_Tx_getAndClearIrq();
+uint32_t Spw_Tx_getAndClearIrq(const Spw_Tx* const tx);
 
 /// \brief Gets and clears masked PktTx interrupts.
-uint32_t Spw_Tx_getAndClearIrqMasked();
+uint32_t Spw_Tx_getAndClearIrqMasked(const Spw_Tx* const tx);
 
 /// \brief Clears PktTx interrupts.
-void Spw_Tx_clearIrq();
+void Spw_Tx_clearIrq(const Spw_Tx* const tx);
 
 /// \brief Decodes PktTx interrupt status register to a structure.
-void Spw_Tx_getStructuredIrq();
+void Spw_Tx_getStructuredIrq(const uint32_t linkIrqStatusValue, Spw_Tx_IrqStatus* const status);
 
 /// \brief Gets PktTx status register.
-void Spw_Tx_getStatus();
+void Spw_Tx_getStatus(Spw_Tx* const tx, Spw_Tx_Status* const status);
 
 #ifdef __cplusplus
 } // extern "C"
